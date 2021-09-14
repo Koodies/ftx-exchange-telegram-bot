86d8cc92a9bc56a7de692278c2ec011d67abd89b
Koodies Tan Ke Wei <kwtan@outlook.sg>
2021-09-14 14:07:35 +0800

    Add Post Request

--- src/ftx/authentication.js
+++ src/ftx/authentication.js
@@ -24,7 +24,7 @@ class Authentication {
         }
         return crypto.createHmac('sha256', process.env.FTX_SECRET).update(timeStamp + method + path + payload).digest('hex')
     }
-    
+
     static sendReq(signature, timeStamp, path) {
         axios.defaults.headers.common['FTX-SIGN'] = signature
         axios.defaults.headers.common['FTX-TS'] = timeStamp
@@ -34,7 +34,22 @@ class Authentication {
             }
         ).catch(
             error => {
-                //console.log(error)
+                console.log(error)
+            }
+        )
+    }
+
+    static sendPostReq(signature, timeStamp, path, data) {
+        axios.defaults.headers.common['FTX-SIGN'] = signature
+        axios.defaults.headers.common['FTX-TS'] = timeStamp
+        return axios.post(`https://ftx.com/api/${path}`, data).then(
+            res => {
+                return { error: !res.data.success, data: res.data.result }
+            }
+        ).catch(
+            error => {
+                console.log(error)
+                return { error: true, data: error }
             }
         )
     }
