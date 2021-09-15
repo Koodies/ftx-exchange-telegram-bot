0bcbebbbc8e0beb59b5c8bca2c2ae40c23b0fa2e
Koodies Tan Ke Wei <kwtan@outlook.sg>
2021-09-15 12:18:46 +0800

    Display more info & beautify balance msg

--- src/ftx/wallet.js
+++ src/ftx/wallet.js
@@ -6,18 +6,12 @@ const ftx = require('./authentication')
 class Wallet {
     static async getBalances() {
         try {
-            let result = []
             const timeStamp = +new Date
             const path = `wallet/balances`
             const signature = ftx.generateSignature('GET', timeStamp, path)
-            let balances = await ftx.sendReq(signature, timeStamp, path)
-            balances.forEach(balance => {
-                if (balance.total === 0) return
-                result.push(`${balance.coin}: ${balance.total} - USD$${balance.usdValue.toFixed(2)}\n`)
-            })
-            return result
+            return await ftx.sendReq(signature, timeStamp, path)
         } catch (error) {
-            //console.log(error)
+            return []
         }
     }
 
