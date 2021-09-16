5e4bb59e9e49365c89da2553fa18a25b28938d36
Koodies Tan Ke Wei <kwtan@outlook.sg>
2021-09-16 15:22:53 +0800

    ADD try catch on localDB controller

--- src/controller/localDB.js
+++ src/controller/localDB.js
@@ -4,19 +4,31 @@ const spotMargin = require('../ftx/spotMargin')
 
 class LocalDB {
     static async getLendingCoinDatabase() {
-        let rateRes = await spotMargin.getRates()
-        let coinRes = await wallet.getCoins()
-        if(rateRes.error || coinRes.error) return
-        let listOfRates = rateRes.data
-        let listOfCoins = coinRes.data
-        let result = []
-        listOfRates.forEach(rate => {
-            let index = listOfCoins.findIndex(coin => coin['id'].match(rate['coin']))
-            let { id, name, tokenizedEquity = false } = listOfCoins[index]
-            result.push({id, name, tokenizedEquity})
-        })
-        return result
+        try {
+            let rateRes = await spotMargin.getRates()
+            let coinRes = await wallet.getCoins()
+            if(rateRes.error || coinRes.error) return
+            let listOfRates = rateRes.data
+            let listOfCoins = coinRes.data
+            let result = []
+            listOfRates.forEach(rate => {
+                let index = listOfCoins.findIndex(coin => coin['id'].match(rate['coin']))
+                let { id, name, tokenizedEquity = false } = listOfCoins[index]
+                result.push({id, name, tokenizedEquity})
+            })
+            return result
+        } catch (error) {
+            
+        }
     }
+
+    static doesCoinExist(coin) {
+        try {
+            
+        } catch (error) {
+            
+        }
+    } 
 }//end of LocalDB
 
 module.exports = LocalDB
