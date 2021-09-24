739788e14cabda5944627c8c9ef792eb751174d1
Koodies Tan Ke Wei <kwtan@outlook.sg>
2021-09-24 11:14:13 +0800

    ADD file controller

---
+++ src/controller/file.js
@@ -0,0 +1,56 @@
+'use strict'
+const fs = require('fs')
+const filePath = `${__dirname}/../../database.json`
+const file = require(filePath)
+
+class File {
+    static addtoWatchlist(coin) {
+        file.watchlist.push(coin)
+        saveDB(file)
+    }
+
+    static addToLendingList(coin) {
+        file.lending.push(coin)
+        saveDB(file)
+    }
+
+    static rmFromWatchlist(coin) {
+        const index = file.watchlist.indexOf(coin)
+        if (index === -1) {
+            ctx.reply(`${coin} is not in the list`)
+            return
+        }
+        file.watchlist.splice(index, 1)
+        saveDB(file)
+    }
+
+    static rmFromLendinglist(coin) {
+        const index = file.lending.indexOf(coin)
+        if (index === -1) {
+            ctx.reply(`${coin} is not in the lending list`)
+            return
+        }
+        file.watchlist.splice(index, 1)
+        saveDB(file)
+    }
+
+    static updateDB(coinsJSON) {
+        file['db'] = coinsJSON
+        file['lastUpdated'] = Date.now()
+        saveDB(file)
+    }
+
+    static saveLogs(logs) {
+        file.logs.push(logs)
+        saveDB(file)
+    }
+}//end of LocalDB
+
+function saveDB(newFile) {
+    fs.writeFile(filePath, JSON.stringify(newFile), (err) => {
+        if (err) return console.log(err)
+        console.log(`Successfully saved database.json`)
+    })
+}
+
+module.exports = File
