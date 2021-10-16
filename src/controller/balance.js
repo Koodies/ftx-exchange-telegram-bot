729db1d65b869bfec0e0f9e4b8e1f489bb504a2b
Koodies Tan Ke Wei <kwtan@outlook.sg>
2021-10-16 14:41:33 +0800

    Clean balance.js

--- src/controller/balance.js
+++ src/controller/balance.js
@@ -5,24 +5,24 @@ class Balance {
     static async getBalance() {
         try {
             let res = await wallet.getBalances()
-            if(res.error) return `No Balance Found`
-            return generateBalanceSheet(res.data)
+            if(res.error) throw new Error(res.error)
+            return genBalanceSheet(res.data)
         } catch (error) {
             console.log(`Error: ${error}`)
-            return `No Balances Found`
+            return `No balances found`
         }
     }
 }//end of Balance
 
-function generateBalanceSheet(arrayOfBalance) {
+function genBalanceSheet(balances) {
     let message = `Balances: \n`
     let counter = 0
-    arrayOfBalance.forEach(balance => {
+    balances.forEach(balance => {
         if(balance.total === 0) return
         counter++
         message += `[${balance.coin}] Total: ${balance.total}, Value: USD$${balance.usdValue.toFixed(2)} \n`
     })
-    return (counter === 0) ? `No Balances` : message
+    return (counter === 0) ? `No balances found` : message
 }
 
 module.exports = Balance
