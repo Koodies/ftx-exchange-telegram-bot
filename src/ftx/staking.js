99b47bdd27dfbdb50e519d2f2c28d4dd31bc3d30
Koodies Tan Ke Wei <kwtan@outlook.sg>
2021-10-17 12:49:11 +0800

    Add ftx/staking.js

---
+++ src/ftx/staking.js
@@ -0,0 +1,27 @@
+const ftx = require('./authentication')
+
+class Staking {
+    /**
+     * Get stakes on FTX
+     */
+    static getStakesBalance() {
+        let timeStamp = +new Date
+        let path = `staking/balances`
+        let signature = ftx.generateSignature('GET', timeStamp, path)
+        return ftx.sendReq(signature, timeStamp, path)
+    }
+
+    /**
+     * Send a staking request
+     * @param  {String} coin    Ticker Symbol
+     * @param  {Integer} size   
+     */
+    static sendStakeReq(coin, size) {
+        let timeStamp = +new Date
+        let path = `srm_stakes/stakes`
+        let signature = ftx.generateSignature('POST', timeStamp, path, {coin, size})
+        return ftx.sendPostReq(signature, timeStamp, path, data)
+    }
+}// end of Staking
+
+module.exports = Staking
