b7a8442e56a3fe6fcd5e64ba72f49c9f8b0d89ae
Koodies Tan Ke Wei <kwtan@outlook.sg>
2021-11-13 12:39:49 +0800

    clean

--- src/controller/logs.js
+++ src/controller/logs.js
@@ -27,11 +27,7 @@ function genLendLogMsg(logs) {
     let msg = `-------------------------------------\nTime: ${new Date(logs['timestamp']).toLocaleString()}\n`
     logs['lend'].forEach(log => {
         const { lendOut, coin, balance, error } = log
-        if(lendOut) {
-            msg += `Success - [${coin}] size: ${balance}\n`
-        } else {
-            msg += `Error - [${coin}] size: ${balance} | ${error}\n`
-        }
+        msg += (lendOut) ? `Success - [${coin}] size: ${balance}\n` : `Error - [${coin}] size: ${balance} | ${error}\n`
     })
     return msg
 }
@@ -40,11 +36,7 @@ function genStakeLogMsg(logs) {
     let msg = `-------------------------------------\nTime: ${new Date(logs['timestamp']).toLocaleString()}\n`
     logs['stake'].forEach(log => {
         const { stake, coin, availableWithoutBorrow, error } = log
-        if(stake) {
-            msg += `Success - [${coin}] size: ${availableWithoutBorrow}\n`
-        } else {
-            msg += `Error - [${coin}] | ${error}\n`
-        }
+        msg += (stake) ? `Success - [${coin}] size: ${availableWithoutBorrow}\n` : `Error - [${coin}] | ${error}\n`
     })
     return msg
 }
